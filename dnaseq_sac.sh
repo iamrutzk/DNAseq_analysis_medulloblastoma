6d95a345fbac34aa5ee34ddf73ddbba2f76a4554
Amrutha Kenche <amruthakenche@gmail.com>
2025-12-31 14:02:37 +0530

    Add files via upload

---
+++ dnaseq_sac.sh
@@ -0,0 +1,7 @@
+##STEP1: Data Downloading
+
+wget -c ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR152/056/SRR15299556/SRR15299556_1.fastq.gz
+wget -c ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR152/056/SRR15299556/SRR15299556_2.fastq.gz
+
+#Ref genome
+wget -c https://hgdownload.soe.ucsc.edu/goldenPath/sacCer3/bigZips/sacCer3.fa.gz
